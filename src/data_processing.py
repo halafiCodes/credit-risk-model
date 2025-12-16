e89509d2a61b5162561f28778971b51ab25fef46
Halafi <weldesilassieredae@gmail.com>
2025-12-16 15:23:17 +0000

    Task4: Created proxy target variable for high-risk customers

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
-
+import joblib
+import logging
+import sys
+import warnings
 def extract_time_features(df):
     df['TransactionStartTime'] = pd.to_datetime(df['TransactionStartTime'])
     df['hour'] = df['TransactionStartTime'].dt.hour
